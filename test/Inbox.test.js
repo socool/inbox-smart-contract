4e5f6e5101fe475ec2f1d86e78afef09c9ffd882
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2021-04-07 16:42:53 +0700

    add car class

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,3 +2,13 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+class Car{
+    park(){
+        return "stopped";
+    }
+
+    drive(){
+        return "vroom";
+    }
+}
