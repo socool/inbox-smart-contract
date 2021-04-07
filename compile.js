1a446b29db977205e5afbbe60cabe6a74c3f88ba
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2021-04-07 14:37:41 +0700

    npm install

--- compile.js
+++ compile.js
@@ -3,7 +3,7 @@ const path = require('path');// for get cross platfrom when get path
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'constracts','Inbox.sol');
+const inboxPath = path.resolve(__dirname, 'contracts','inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 console.log(solc.compile(source, 1));
