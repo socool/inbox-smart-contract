be47b62517f6efa4a6b39398adb657d9c76d2d6f
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2021-04-08 16:43:21 +0700

    Web3 Version fix

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const {interface, bytecode} = require('../compile');
 
 class Car{
